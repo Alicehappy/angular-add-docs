d1a3bda00cf29539e84b139f34b4cf0a4ff220bd
Alice Zou <alice@datafax.com>
2020-01-31 14:23:00 -0500

    Added Template Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,9 +12,9 @@ import { CheckPwdDirective } from './check-pwd.directive';
 import { AddDocsComponent } from './add-docs/add-docs.component';
 import { CheckPwdContentComponent, CheckPwdContentCompComponent } from './check-pwd-content/check-pwd-content.component';
 import { ChangePwdDirectiveComponent } from './change-pwd-directive/change-pwd-directive.component';
-import { FormsModule } from '@angular/forms';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ProfileEditorComponent } from './profile-editor/profile-editor.componen
     CheckPwdContentCompComponent,
     ChangePwdDirectiveComponent,
     NameEditorComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    HeroFormComponent
   ],
   imports: [
     BrowserModule,
